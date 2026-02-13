a2dde7dc3f282fc035693e9a3cb6a0616665ca0e
Juan Cardenas <jacardenas@outlook.com>
2026-02-13 09:09:54 -0600

    gestion pagos

--- capacitacion/.htaq5tbszdgc8y.data/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'i76QVjpg38fBJ2Ta9Fhtk0yjai1GN5s5rhfarma.mx';
-$CFG->bootstraphash = '607a95c393750de59a149925a57867cc';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
